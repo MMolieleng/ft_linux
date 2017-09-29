5b2eb20b37ee78709b00c3b409e352eda02b5b73
MMolieleng <mjmolieleng@gmail.com>
2017-09-29 16:20:06 +0200

    The script installs necessary packages for the kernel to compile

---
+++ config_packages.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# The script is for installing all the necessary scripts for the kernel to be compiled
+
+sudo apt install m4 		# Installs the m4 package
+sudo apt install gawk		# Installs the gawk package
+sudo apt install byacc flex	# Installs yacc and lexa
+sudo apt install textinfo	# installs makeinfo package
