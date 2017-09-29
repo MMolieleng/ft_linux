6e0c0ae264114a41bac8910cc47dca0b43e9c6b9
MMolieleng <mjmolieleng@gmail.com>
2017-09-29 19:29:38 +0200

    Added file system creation script

---
+++ commands.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# The script is for creating the actual Linux File System
+
+sudo mkfs -v -t ext4 /dev/sdb		# To create an ext4 file system on the LFS partition
+
+# End of the script
--- config_packages.sh
+++ config_packages.sh
@@ -2,7 +2,11 @@
 
 # The script is for installing all the necessary scripts for the kernel to be compiled
 
+sudo ln -sf bash /bin/sh	# Link bash to /bin/sh
 sudo apt install m4 		# Installs the m4 package
 sudo apt install gawk		# Installs the gawk package
 sudo apt install byacc flex	# Installs yacc and lexa
 sudo apt install textinfo	# installs makeinfo package
+
+# This should be the last thing 
+sudo ln -sf dash /bin/sh
